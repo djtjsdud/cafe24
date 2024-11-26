67f2c238549c0d66328d46ae99c7d4cc8f9ba55a
djtjsdud <djwnsdn@gmail.com>
2024-11-26 19:57:30 +0900

    feat : 부트스트랩 추가, 신상품 영역 탭 생성, 리미티드 에디션 영역 생성 및 스타일 추가

--- skin2/layout/basic/js/eo.js
+++ skin2/layout/basic/js/eo.js
@@ -3,6 +3,10 @@
 
 var swiper = new Swiper(".mySwiper", {
     loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     pagination: {
         el: ".swiper-pagination",
         type: "fraction",
@@ -10,5 +14,15 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+});
+
+var newSwiper = new Swiper(".mySwiper.new-swiper", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: ".new-swiper .swiper-pagination",
+        clickable: true,
     },
 });
