968660d6a8741a286e979ae9478e05f2779351ce
djtjsdud <djwnsdn@gmail.com>
2024-12-04 19:26:50 +0900

    docs : 러쉬 랭킹 수정, 이미지 추가, 상품 카테고리 추가

--- skin2/layout/basic/js/eo.js
+++ skin2/layout/basic/js/eo.js
@@ -20,12 +20,42 @@ var swiper = new Swiper("#main_slide.mySwiper", {
 // 신상품 슬라이드
 
 setTimeout(()=> {
-    const newSwiper = new Swiper("#bath .new-swiper", {
+    const bathSwiper = new Swiper("#bath .bath-swiper", {
         loop: true,
         slidesPerView: 'auto',
         spaceBetween: 20, 
         pagination: {
             el: "#bath .swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    const showerSwiper = new Swiper("#shower .shower-swiper", {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 20, 
+        pagination: {
+            el: "#shower .swiper-pagination",
+            clickable: true,
+        },
+    });
+    
+    const bodySwiper = new Swiper("#body .body-swiper", {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 20, 
+        pagination: {
+            el: "#body .swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    const hairSwiper = new Swiper("#hair .hair-swiper", {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 20, 
+        pagination: {
+            el: "#hair .swiper-pagination",
             clickable: true,
         },
     });
