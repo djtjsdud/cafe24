fc5da709e56a444c475b3d74865c10c51f7989ee
djtjsdud <djwnsdn@gmail.com>
2024-12-11 17:13:26 +0900

    feat : 러쉬  유튜브 채널 영역 추가

--- skin2/layout/basic/js/eo.js
+++ skin2/layout/basic/js/eo.js
@@ -59,6 +59,17 @@ setTimeout(()=> {
             clickable: true,
         },
     });
+
+    const giftSwiper = new Swiper("#gift-swiper .mySwiper", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 24,
+        centeredSlides: true,
+        navigation: {
+            nextEl: "#gift-swiper .swiper-button-next",
+            prevEl: "#gift-swiper .swiper-button-prev",
+        },
+    });
 }, 500);
 
 // 미들배너 슬라이드
@@ -83,16 +94,3 @@ var reviewSwiper = new Swiper("#review-swiper .mySwiper", {
         prevEl: "#review-swiper .swiper-button-prev",
     },
 });
-
-// 기프트 슬라이드
-
-var giftSwiper = new Swiper("#gift-swiper .mySwiper", {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 24,
-    centeredSlides: true,
-    navigation: {
-        nextEl: "#gift-swiper .swiper-button-next",
-        prevEl: "#gift-swiper .swiper-button-prev",
-    },
-}, 500);
